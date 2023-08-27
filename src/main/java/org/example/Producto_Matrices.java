d164d2df6aed036b169001ee85d4514c1035fc32
LuzRdgz <luzrdgz15@gmail.com>
2023-08-27 00:32:18 -0700

    Ejercicio I y II

--- src/main/java/org/example/Producto_Matrices.java
+++ src/main/java/org/example/Producto_Matrices.java
@@ -1,11 +1,9 @@
 package org.example;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
 
 public class Producto_Matrices {
 
@@ -22,21 +20,48 @@ public class Producto_Matrices {
             cantRenMA = fileA.read();
             cantRenMB = fileB.read();
 
-            System.out.println("Renglon a =" + cantRenMA);
-            System.out.println("Renglon b =" + cantRenMB);
+            /*System.out.println("Renglon a =" + cantRenMA);
+            System.out.println("Renglon b =" + cantRenMB);*/
 
             cantColMA = fileA.read();
             cantColMB = fileB.read();
 
-            System.out.println("Columna a =" + cantColMA);
-            System.out.println("columna b =" + cantColMB);
+            int[][] matA = new int[cantRenMA][cantColMA];
+            int[][] matB = new int[cantRenMB][cantColMB];
+            int[][] matC = new int[cantRenMA][cantColMB];
 
+           /* System.out.println("Columna a =" + cantColMA);
+            System.out.println("columna b =" + cantColMB);*/
+
+            for (int i = 0; i< matA.length; i++){
+                for (int j = 0; j< matA.length; j++){
+                    matA[i][j] = fileA.read();
+                }
+            }
+            for (int i = 0; i< matB.length; i++){
+                for (int j = 0; j< matB.length; j++){
+                    matB[i][j] = fileB.read();
+                }
+            }
             fileA.close();
             fileB.close();
 
+            if (cantColMA == cantRenMB){
+                for (int i = 0; i< matA.length; i++){
+                    for (int j =0; j< matB[0].length; j++){
+                        for (int k = 0; k < matA[0].length; k++){
+                            matC[i][j] = (matA[i][k]) * (matB[k][j]);
+                        }
+                    }
+                }
+            }
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            for (int[] ints : matC) {
+                for (int m = 0; m < matC.length; m++) {
+                    System.out.print(ints[m]);
+                }
+            }
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
