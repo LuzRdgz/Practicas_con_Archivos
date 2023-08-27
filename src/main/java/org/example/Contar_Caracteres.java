61add0557dadeec08fb81904da8c9fddda9f3077
LuzRdgz <luzrdgz15@gmail.com>
2023-08-26 23:56:13 -0700

    Ejercicio I y II

--- src/main/java/org/example/Contar_Caracteres.java
+++ src/main/java/org/example/Contar_Caracteres.java
@@ -1,9 +1,8 @@
 package org.example;
 
-import java.io.BufferedReader;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.Scanner;
 
 
@@ -15,52 +14,43 @@ public class Contar_Caracteres {
         Scanner scan;
         String palabra;
         int longitud;
-        int histograma[] = new int[9]; // para los contadores
+        int [] histograma = new int[9];
 
         try {
             scan = new Scanner(new File(archivo));
 
-            while (scan.hasNext()){
+            while (scan.hasNext()) {
                 palabra = scan.next();
 
-                for (int i = 0; i<palabra.length(); i++) {
-                        longitud = palabra.length();
-                        if (!Character.isDigit(i)){
-                            if (longitud >= 2 && longitud <= 10){
-                                switch (longitud){
-                                    case 2: histograma[0] ++;
-                                        break;
-                                    case 3: histograma[1] ++;
-                                        break;
-                                    case 4: histograma[2] ++;
-                                        break;
-                                    case 5: histograma[3] ++;
-                                        break;
-                                    case 6: histograma[4] ++;
-                                        break;
-                                    case 7: histograma[5] ++;
-                                        break;
-                                    case 8: histograma[6] ++;
-                                        break;
-                                    case 9: histograma[7] ++;
-                                        break;
-                                    case 10: histograma[8] ++;
-                                        break;
-                                    default:
-                                        // System.out.println("ES MENOR A DOS CARACTERES. ES MAYOR A 10 CARACTERES. ES UN NUMERO.");
+                for (int i = 0; i < palabra.length(); i++) {
+                    longitud = palabra.length();
+                    if (!Character.isDigit(i)) {
+                        if (longitud >= 2 && longitud <= 10) {
+                            switch (longitud) {
+                                case 2 -> histograma[0]++;
+                                case 3 -> histograma[1]++;
+                                case 4 -> histograma[2]++;
+                                case 5 -> histograma[3]++;
+                                case 6 -> histograma[4]++;
+                                case 7 -> histograma[5]++;
+                                case 8 -> histograma[6]++;
+                                case 9 -> histograma[7]++;
+                                case 10 -> histograma[8]++;
+                                default -> {
                                 }
                             }
                         }
-
+                    }
                 }
-                }
-            } catch (FileNotFoundException e) {
+            }
+           scan.close();
+        } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
 
         System.out.println("HISTOGRAMA");
         for (int i = 0; i<histograma.length; i++){
-            System.out.println("Palabras con " + (i+2) + " caracteres = " + histograma[i] + " veces.");
+            System.out.println("Palabras con " + (i+2) + " caracteres = " + histograma[i] + " palabras.");
         }
     }
 }
--- src/main/java/org/example/Producto_Matrices.java
+++ src/main/java/org/example/Producto_Matrices.java
@@ -1,8 +1,47 @@
 package org.example;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Producto_Matrices {
 
-    final String matrizA = "C:\\Users\\LuzRd\\Desktop\\divina_comedia_sp.txt";
-    final String matrizB = "C:\\Users\\LuzRd\\Desktop\\divina_comedia_sp.txt";
+    static final String matrizA = "C:\\Users\\LuzRd\\Desktop\\a.mat";
+    static final String matrizB = "C:\\Users\\LuzRd\\Desktop\\b.mat";
+    static int cantRenMA, cantRenMB, cantColMA, cantColMB;
+
+    public static void main(String[] args) {
+
+        try {
+            FileInputStream fileA = new FileInputStream(matrizA);
+            FileInputStream fileB = new FileInputStream(matrizB);
+
+            cantRenMA = fileA.read();
+            cantRenMB = fileB.read();
+
+            System.out.println("Renglon a =" + cantRenMA);
+            System.out.println("Renglon b =" + cantRenMB);
+
+            cantColMA = fileA.read();
+            cantColMB = fileB.read();
+
+            System.out.println("Columna a =" + cantColMA);
+            System.out.println("columna b =" + cantColMB);
+
+            fileA.close();
+            fileB.close();
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 
 }
